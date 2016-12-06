45324cb78b760851564bce7a3cef90e3c8266e1d
Aalind Singh <singh.aalind@gmail.com>
2016-12-06 17:06:29 +0530

    Update recipe_1.py

--- recipe_1.py
+++ recipe_1.py
@@ -1,6 +1,23 @@
+import numpy as np
+#importing iris dataset which comes preloaded with sklearn
+from sklearn.datasets import load_iris
 from sklearn import tree
-features = [[140, 1], [130, 1], [150, 0], [170, 0]]
-labels = [0, 0, 1, 1]
+
+iris = load_iris()
+#creating a fold dataset for testing the classifier later
+test_idx = [0,50,100]
+
+# training data
+train_target = np.delete(iris.target, test_idx)
+train_data = np.delete(iris.data, test_idx, axis=0)
+
+#testing data
+test_target = iris.target[test_idx]
+test_data = iris.data[test_idx ]
+
+#training the classifier
 clf = tree.DecisionTreeClassifier()
-clf = clf.fit(features, labels)
-print clf.predict([[150, 0]])
+clf.fit(train_data, train_target)
+
+print (test_target)
+print (clf.predict(test_data))
